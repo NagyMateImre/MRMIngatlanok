7218a8993adb24b83fdc51257fb9788790862fa9
NagyMateImre <nagym3@kkszi.hu>
2025-11-25 22:27:33 +0100

    Végső simitások (oldal stilusának igazitása + csapattag kártyák)

--- js/data.js
+++ js/data.js
@@ -156,3 +156,17 @@ let Tomb = [
 
 localStorage.setItem("Tomb",JSON.stringify(Tomb))
 localStorage.setItem("osszeingatlan",Tomb.length)
+
+
+const Ingatlan = document.getElementById('Ingatlan');
+const Ugyfel = document.getElementById('Ugyfel');
+const Kezelt = document.getElementById('Kezelt');
+const ertekek = document.getElementById('ertekek');
+const table = document.getElementById('table');
+
+document.addEventListener('DOMContentLoaded', () =>{
+    let Ingatlanossz = Math.round(Math.random()*1000)
+    Ingatlan.innerText = Ingatlanossz;
+    Ugyfel.innerText = Ingatlanossz+ Math.round(Math.random()*30);
+    Kezelt.innerText = localStorage.getItem("osszeingatlan");
+});
